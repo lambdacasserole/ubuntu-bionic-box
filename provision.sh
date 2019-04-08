9b406416751bdbe578b5c70b1e26ee841f5c6c08
Saul Johnson <saul.a.johnson@gmail.com>
2019-04-08 14:46:52 +0100

    Add Vagrantfile and provisioning script

---
+++ provision.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install desktop system.
+add-apt-repository -y ppa:gnome3-team/gnome3
+apt-get update -y
+apt-get install -y gnome-shell ubuntu-gnome-desktop
+apt-get upgrade -y
+
+# Install guest additions.
+wget https://download.virtualbox.org/virtualbox/6.0.4/VBoxGuestAdditions_6.0.4.iso
+mkdir /media/GuestAdditionsISO
+mount -o loop VBoxGuestAdditions_6.0.4.iso /media/GuestAdditionsISO
+/media/GuestAdditionsISO/VBoxLinuxAdditions.run -- --force
